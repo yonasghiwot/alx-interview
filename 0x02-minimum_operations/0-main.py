118a353007e7ff2c59dcfff913280da58c7046ba
Yonas Gebrehiwot <yonihabeshaw@gmail.com>
2024-10-24 15:52:55 +0300

    0-main.py

---
+++ 0x02-minimum_operations/0-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+File for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
